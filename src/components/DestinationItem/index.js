3af60abe5962154ed73e974f74726c5a02a865c5
SachinBalagam <balagamsachin337@gmail.com>
2023-07-28 06:01:43 +0000

    Final Commit

--- src/components/DestinationItem/index.js
+++ src/components/DestinationItem/index.js
@@ -1,1 +1,17 @@
 // Write your code here
+
+import './index.css'
+
+const DestinationItem = props => {
+  const {destinationsList} = props
+  const {imgUrl, name} = destinationsList
+
+  return (
+    <li className="each-list">
+      <img src={imgUrl} alt={name} className="destination-image" />
+      <p className="destination-name">{name}</p>
+    </li>
+  )
+}
+
+export default DestinationItem
--- src/components/DestinationSearch/index.js
+++ src/components/DestinationSearch/index.js
@@ -1,1 +1,47 @@
 // Write your code here
+import {Component} from 'react'
+import DestinationItem from '../DestinationItem'
+import './index.css'
+
+class DestinationSearch extends Component {
+  state = {searchInput: ''}
+
+  onInputChange = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  render() {
+    const {searchInput} = this.state
+    const {destinationsList} = this.props
+    const searchResults = destinationsList.filter(eachUser =>
+      eachUser.name.toLowerCase().includes(searchInput.toLowerCase()),
+    )
+    return (
+      <div className="bg-container">
+        <h1 className="main-heading">Destination Search</h1>
+        <div className="input-container">
+          <input
+            type="search"
+            placeholder="search"
+            className="input-box"
+            onChange={this.onInputChange}
+            value={searchInput}
+          />
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/destinations-search-icon-img.png "
+            alt="search icon"
+            className="search-image"
+          />
+        </div>
+
+        <ul className="list-container">
+          {searchResults.map(each => (
+            <DestinationItem destinationsList={each} key={each.id} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default DestinationSearch
